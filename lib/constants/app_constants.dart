f3aaf80001ba6f6b6d18c09fe76fe6a547a610d5
Willie Spence <willie.spence@prevoir.mu>
2025-01-21 17:47:36 +0400

    Updated Application Secrets for Self Regisgtration

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -3,6 +3,8 @@ abstract class ThingsboardAppConstants {
   static final thingsboardOAuth2CallbackUrlScheme =
       'org.thingsboard.pe.app.auth';
 
-  static const thingsboardIOSAppSecret = 'Your app secret here';
-  static const thingsboardAndroidAppSecret = 'Your app secret here';
+  static const thingsboardIOSAppSecret =
+      'cVJFN1BTZERLTEd3dm00cmlOVTk2R2N2dmJqazl2eEpvN3JISnR3ZHBCdDg4R3F2RGJQeWhZWndFY3RDVHB3bA==';
+  static const thingsboardAndroidAppSecret =
+      'VTNrcWVMSW80eVpGVEl6Y05GUDVLZlc2bTFmeGUyWXRkMWFBRTVOcUo4QVRaUXdSSm1YWEIwZWxXQ2pmaEQxSA==';
 }
