a9d2c77adb275a1e1413348043e210e885e336c5
Willie Spence <willie@forty2.tech>
2025-08-22 16:38:36 +0400

    Commit Custom Endpoint

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,7 +1,8 @@
 abstract final class ThingsboardAppConstants {
-  static const thingsBoardApiEndpoint = String.fromEnvironment('thingsboardApiEndpoint');
+  //static const thingsBoardApiEndpoint = String.fromEnvironment('thingsboardApiEndpoint');
+  static const thingsBoardApiEndpoint = "https://portal.omnithings.solutions";
   static const thingsboardOAuth2CallbackUrlScheme = String.fromEnvironment('thingsboardOAuth2CallbackUrlScheme');
   static const thingsboardIOSAppSecret = String.fromEnvironment('thingsboardIosAppSecret');
   static const thingsboardAndroidAppSecret = String.fromEnvironment('thingsboardAndroidAppSecret');
-  static const ignoreRegionSelection = thingsBoardApiEndpoint != 'https://thingsboard.cloud';
+  static const ignoreRegionSelection = thingsBoardApiEndpoint != '';
 }
