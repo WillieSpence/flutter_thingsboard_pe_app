dd9f0f9a64c91c97b7114f6ec3d67066a81979a9
Willie Spence <willie@forty2.tech>
2025-08-22 16:07:27 +0400

    Commit

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -3,5 +3,5 @@ abstract final class ThingsboardAppConstants {
   static const thingsboardOAuth2CallbackUrlScheme = String.fromEnvironment('thingsboardOAuth2CallbackUrlScheme');
   static const thingsboardIOSAppSecret = String.fromEnvironment('thingsboardIosAppSecret');
   static const thingsboardAndroidAppSecret = String.fromEnvironment('thingsboardAndroidAppSecret');
-  static const ignoreRegionSelection = thingsBoardApiEndpoint != '';
+  static const ignoreRegionSelection = thingsBoardApiEndpoint != 'https://portal.omnithings.solutions';
 }
