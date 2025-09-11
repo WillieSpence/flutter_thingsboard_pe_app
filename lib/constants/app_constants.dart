bf04a118ae39f3739938ebc40a110d3d9ff76200
Willie Spence <willie@forty2.tech>
2025-09-11 20:41:09 +0400

    Bug Fix

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,6 +1,6 @@
 abstract final class ThingsboardAppConstants {
-  //static const thingsBoardApiEndpoint = String.fromEnvironment('thingsboardApiEndpoint');
-  static const thingsBoardApiEndpoint = "https://portal.omnithings.solutions";
+  static const thingsBoardApiEndpoint = String.fromEnvironment('thingsboardApiEndpoint');
+  //static const thingsBoardApiEndpoint = "https://portal.omnithings.solutions";
   static const thingsboardOAuth2CallbackUrlScheme = String.fromEnvironment('thingsboardOAuth2CallbackUrlScheme');
   static const thingsboardIOSAppSecret = String.fromEnvironment('thingsboardIosAppSecret');
   static const thingsboardAndroidAppSecret = String.fromEnvironment('thingsboardAndroidAppSecret');
